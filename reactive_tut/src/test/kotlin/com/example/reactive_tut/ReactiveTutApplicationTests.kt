29ca19fd325c745b619baf35e697a6bde010a5dc
Khoa Hoang <whoskhoahoang@gmail.com>
2020-07-07 23:06:39 +0900

    Complete Tutorial 1

---
+++ reactive_tut/src/test/kotlin/com/example/reactive_tut/ReactiveTutApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.reactive_tut
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ReactiveTutApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
